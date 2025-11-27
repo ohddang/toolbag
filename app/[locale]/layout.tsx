ddb5574138a527176d3ab9513630fc18d53d0b5d
ohddang <ohddang509@gmail.com>
2025-11-28 01:14:39 +0900

    feat: 구글 애널리틱스 태그 추가

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -157,6 +157,16 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXH4PD5T');`,
+          }}
+        />
         <link rel="canonical" href={`https://toolbag.vercel.app/${locale}`} />
         <link rel="alternate" hrefLang="ko" href="https://toolbag.vercel.app/ko" />
         <link rel="alternate" hrefLang="en" href="https://toolbag.vercel.app/en" />
@@ -175,6 +185,15 @@ export default async function LocaleLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXH4PD5T"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Analytics />
         {children}
       </body>
